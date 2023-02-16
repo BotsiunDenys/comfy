35aace897a766da9d1db2ae80204fc986dc012f0
Denys Botsyun <denwa62@gmail.com>
2023-02-16 17:02:45 +0200

    fixed troubles with displaying

---
+++ scripts/about.js
@@ -0,0 +1,8 @@
+import { displayCart, hideCart, cartIcon, closeCart } from './displayCart.js';
+import { checkBox, checkBoxChange, resize } from './burgerMenu.js';
+cartIcon.addEventListener('click', () => {
+  displayCart();
+});
+hideCart.addEventListener('click', () => closeCart());
+checkBox.addEventListener('change', () => checkBoxChange());
+window.addEventListener('resize', () => resize());
